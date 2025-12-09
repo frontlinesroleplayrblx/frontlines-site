14f2ee3f16d38974ca6118a121de744c26cf8ae7
. <.>
2025-12-09 23:20:39 +0000

    nfeuinf

--- frontend/src/pages/StartPage.js
+++ frontend/src/pages/StartPage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../styles.css';
 
 export default function StartPage() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Frontlines Roleplay CAD</h1>
+      <h1>Frontlines Roleplay</h1>
       <p>
         <Link to="/login">Login</Link>
       </p>

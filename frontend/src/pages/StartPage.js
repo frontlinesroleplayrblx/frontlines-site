b81029ecf75de7497d3bb8c1b001c68ee12ad415
. <.>
2025-12-13 09:44:35 +0000

    jdjdj

--- frontend/src/pages/StartPage.js
+++ frontend/src/pages/StartPage.js
@@ -7,11 +7,7 @@ import Footer from "../components/Footer";
 export default function StartPage() {
   return (
     <div className="min-h-screen bg-zinc-950 text-zinc-100">
-      <Navbar />
-      <Hero />
-      <Marketplace />
-      <CTA />
-      <Footer />
+      <p>test</p>
     </div>
   );
 }

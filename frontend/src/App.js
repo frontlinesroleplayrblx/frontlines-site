a6cb667a8399e8c150af4348a635cfabf1eea11b
. <.>
2025-12-13 09:02:39 +0000

    binon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,6 @@ export default function App() {
 }
 */
 
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import LoginPage from "./pages/LoginPage";

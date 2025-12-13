f4226bc6c9840d2429ac760c271ceb9a1149076e
. <.>
2025-12-13 21:31:37 +0000

    chnaged sheet

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 SPREADSHEET_ID = "1iD_5fdFBoD8rsyDywGcWxaPe3xOJkLKppb2YWZWGcEk"
-RANGE_NAME = "A2:C"
+RANGE_NAME = "A2:B"
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 

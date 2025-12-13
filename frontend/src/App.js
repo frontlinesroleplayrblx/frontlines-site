af2abc5842809d5ddd8844c38cd7f5c607dab167
. <.>
2025-12-13 08:45:03 +0000

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
+
 /*
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import StartPage from "./pages/StartPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
@@ -25,35 +25,17 @@ export default function App() {
         />
       </Routes>
     </Router>
+
   );
 }
-*/
+  */
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StartPage from "./pages/StartPage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import UnitsPage from "./pages/UnitsPage";
-import ProtectedRoute from "./pages/ProtectedRoute";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/units"
-          element={
-            <ProtectedRoute>
-              <UnitsPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
-
+    <div style={{ color: "white", padding: 40 }}>
+      APP IS RENDERING
+    </div>
   );
 }

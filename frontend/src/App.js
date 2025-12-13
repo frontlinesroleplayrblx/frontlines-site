1b66c984032b1d867a81127e123c591a0c262a91
. <.>
2025-12-13 08:59:16 +0000

    undoimefoin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,34 @@
-
 /*
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import StartPage from "./pages/StartPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import UnitsPage from "./pages/UnitsPage";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/units"
+          element={
+            <ProtectedRoute>
+              <UnitsPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+*/
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StartPage from "./pages/StartPage";
@@ -28,11 +57,13 @@ export default function App() {
 
   );
 }
-  */
 
 import React from "react";
-import StartPage from "./pages/StartPage";
 
 export default function App() {
-  return <StartPage />;
+  return (
+    <div style={{ color: "white", padding: 40 }}>
+      APP IS RENDERING
+    </div>
+  );
 }
